34b824cc9cdc3384326ba7ace06351d281534aa6
AndreZigiotti <algazigiotti@gmail.com>
2022-08-13 12:01:22 -0300

    Adding Angular Material + adjusts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout'
 
 @Component({
   selector: 'cv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'montadorCurriculo';
+export class AppComponent implements OnDestroy {
+  mobileQuery: MediaQueryList
+
+  private _mobileQueryListener: () => void
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)')
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges()
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener)
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.mobileQuery?.removeEventListener('change', this._mobileQueryListener)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './modules/components/components.module';
+import { PreviewComponent } from './components/preview/preview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TemplateOneComponent } from './templates/template-one/template-one.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewComponent,
+    TemplateOneComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
